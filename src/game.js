2c75aeaa0a9bc7919c3a9c5644a469c12f9c32e2
“Saki <sakisandra.c@gmail.com>
2023-04-29 16:09:41 -0600

    Start the game

--- src/game.js
+++ src/game.js
@@ -1,3 +1,4 @@
+const { createCard, evaluateGuess, createDeck, countCards, createRound, takeTurn, calculatePercentCorrect } = require('./card');
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
@@ -10,5 +11,28 @@ function printMessage(deck) {
 function printQuestion(round) {
   util.main(round);
 }
+
+// console.log(prototypeQuestions)
+// let cards = []
+// let card = prototypeQuestions.forEach((card) => {
+//   createCard(card.id, card.question, card.answers, card.correctAnswer)
+//   cards.push(card)
+// })
+// console.log(cards)
+
+function start() {
+  let cards = []
+  prototypeQuestions.forEach((card) => {
+    createCard(card.id, card.question, card.answers, card.correctAnswer)
+    cards.push(card)
+  })
+  console.log(cards)
+  let deck = createDeck(cards);
+  let round = createRound(deck)
+  printMessage(deck);
+  printQuestion(round);
+}
+
+start(); 
 
 module.exports = { printMessage, printQuestion };
